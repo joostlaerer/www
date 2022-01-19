7bda42c774ffe6995b359204f26f6f97409bfe71
Joost Gjerdingen <70970439+JoostGjerdingen@users.noreply.github.com>
2022-01-19 14:29:46 +0100

    n

--- Python/API/Riotlolapi/first.py
+++ Python/API/Riotlolapi/first.py
@@ -2,7 +2,7 @@ from riotwatcher import LolWatcher, ApiError
 import pandas as pd
 
 # golbal variables
-api_key = 'RGAPI-f02cc4ed-2fd5-4c35-b0cb-c8dedafced7a'
+api_key = 'RGAPI-22f9a7a6-7147-4961-8b17-13a0cd35b232'
 watcher = LolWatcher(api_key)
 my_region = 'EUW1'
 
@@ -10,6 +10,6 @@ me = watcher.summoner.by_name(my_region, 'Rud0lf2001')
 """ print(me) """
 
 my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
-""" print(my_ranked_stats) """
+print(my_ranked_stats)
 print(type(my_ranked_stats))
 print (my_ranked_stats[0])
